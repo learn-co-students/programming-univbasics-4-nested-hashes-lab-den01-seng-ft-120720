03ab56aba6d39917020e0319af206126efc98a58
Jeff Golden <gjgolden08@gmail.com>
2020-11-19 22:03:03 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
+  programmer_hash[:grace_hopper]
+    
 
 end
 
@@ -36,6 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+  programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +58,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,7 +85,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+      }
+  programmer_hash
 end
 
 def changing_alan
@@ -103,7 +111,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +134,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  programmer_hash
 end
